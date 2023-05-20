3711e1279a68f62ae511479f21dfa96ca33a66e6
Naveennav18 <naveennav545@gmail.com>
2023-05-20 14:21:08 +0530

    New Text

--- src/org/hello/Hello.java
+++ src/org/hello/Hello.java
@@ -1,12 +1,21 @@
 package org.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello {
 
-	
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("Hello");
+		System.out.println("Welcome");
+
+		List li = new ArrayList();
+		li.add(10);
+		li.add(20);
+		li.add(30);
+		li.add(40);
+		System.out.println(li);
 	}
-	
+
 }
